a1da5e04fca3ea53bfb0f2f0e7712bab83559cd2
SymfonyCordova <978451251@qq.com>
2021-02-21 20:31:48 +0800

    biz auto load

--- src/Laravel/config/zler-biz.php
+++ src/Laravel/config/zler-biz.php
@@ -2,14 +2,14 @@
 return array(
      'options' => array(
         'db.options' => array(
-            'dbname' => getenv('DB_NAME') ?: 'biz-framework',
-            'user' => getenv('DB_USER') ?: 'root',
-            'password' => getenv('DB_PASSWORD') ?: '',
-            'host' => getenv('DB_HOST') ?: '127.0.0.1',
-            'port' => getenv('DB_PORT') ?: 3306,
+            'dbname' => env('DB_DATABASE', ''),
+            'user' => env('DB_USERNAME',''),
+            'password' => env('DB_PASSWORD', '') ,
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
             'driver' => 'pdo_mysql',
             'charset' => 'utf8',
         ),
-         
+
     ),
 );
