528e9aef871a5efc4228d8a977c41bcf2392f107
SymfonyCordova <978451251@qq.com>
2021-02-21 21:01:11 +0800

    biz auto load

--- src/Laravel/config/zler-biz.php
+++ src/Laravel/config/zler-biz.php
@@ -1,15 +1,29 @@
 <?php
-return array(
-     'options' => array(
-        'db.options' => array(
-            'dbname' => env('DB_DATABASE', ''),
-            'user' => env('DB_USERNAME',''),
-            'password' => env('DB_PASSWORD', '') ,
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
+return [
+     'options' => [
+        'db.options' => [
+            'dbname' => getenv('DB_DATABASE', ''),
+            'user' => getenv('DB_USERNAME',''),
+            'password' => getenv('DB_PASSWORD', '') ,
+            'host' => getenv('DB_HOST', '127.0.0.1'),
+            'port' => getenv('DB_PORT', '3306'),
             'driver' => 'pdo_mysql',
             'charset' => 'utf8',
-        ),
-
-    ),
-);
+        ],
+        'wechat.options' => [
+            'gzh' => [
+                'app_id' => getenv('gzh_app_id',''),
+                'app_secret' => getenv('gzh_app_secret', ''),
+                'token' => getenv('gzh_token', ''),
+            ],
+            'miniprograme' => [
+                'app_id' => getenv('miniprograme_app_id', ''),
+                'app_secret' => getenv('miniprograme_app_secret', ''),
+            ],
+            'pc' => [
+                'app_id' => getenv('', ''),
+                'app_secret' => getenv('', ''),
+            ]
+        ]
+    ]
+];
